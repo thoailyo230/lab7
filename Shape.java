e881253b05d82f6aca68f8ff8f0e6c7f5325d677
thoailyo230 <125753220+thoailyo230@users.noreply.github.com>
2023-04-07 08:47:02 +0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Excerice3;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Shape {
+    public double getArea();
+    public void getParameter();
+    
+}
